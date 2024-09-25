c60be863136a4f7b62cd39363a4309026296658e
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-09-24 21:03:51 -0300

    As quatro tarefas e o Bônus estão passando corretamente, cumprindo com todos os requisitos solicitados. Nesse commit foi feito algumas pequenas refatorações

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let result = document.getElementById('result')
 let planetData = document.getElementById('planetData')
+let peopleData = document.getElementById('peopleData')
+let allPlanets = document.getElementById('allPlanets')
 
 async function fetchPlanet() {
     let res = await fetch('https://swapi.dev/api/planets/')
     let {results} = await res.json()
     return results;
 }
-
 async function printPlanet() {
     let planets = await fetchPlanet();
     planets.forEach(planet => {
@@ -14,27 +14,45 @@ async function printPlanet() {
         let button = document.createElement('button');
         button.innerHTML = planet.name;
         button.onclick = () => getPlanetData(planet);
-        result.appendChild(button);
+        allPlanets.appendChild(button);
     });
 };
-
 async function searchPlanet() {
     let searchValue = document.getElementById('searchPlanet').value.toLowerCase();
     let planets = await fetchPlanet();
     planets.forEach(planet => {
-        if (planet.name.toLowerCase().includes(searchValue.toLowerCase())) {
+        if (planet.name.toLowerCase().includes(searchValue)) {
             getPlanetData(planet);
         }
     });
 }
-
 function getPlanetData(planet) {
     planetData.innerHTML = `
         <h3>Planeta: ${planet.name}</h3>
-        <p><strong>Clima:</strong> ${planet.climate}</p>
-        <p><strong>População:</strong> ${planet.population}</p>
-        <p><strong>Terreno:</strong> ${planet.terrain}</p>
+        <p>
+            <strong>Clima:</strong> ${planet.climate}<br>
+            <strong>População:</strong> ${planet.population}<br>
+            <strong>Terreno:</strong> ${planet.terrain}
+        </p>
     `;
+    peopleData.innerHTML = `
+        <thead>
+            <tr> <th colspan="2">Habitantes mais Famosos</th> </tr>
+            <tr> <th>Nome</th> <th>Aniversário</th>  </tr>
+        </thead>
+    `;
+    planet.residents.forEach(resident => { peopleApi(resident) });
 };
+async function peopleApi(resident) {
+    let resposta = await fetch(resident)
+    let people = await resposta.json()
 
+    let tr = document.createElement('tr')
+    tr.innerHTML = `
+        <td>${people.name}</td>
+        <td>${people.birth_year}</td>
+    `;
+    peopleData.appendChild(tr)
+}
 printPlanet()
+console.log(fetchPlanet())
